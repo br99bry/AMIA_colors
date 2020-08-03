93108dc535c5b91e777152a6ba9a01ab9c5abf91
Bryan <bryanblack1999@gmail.com>
2020-08-02 19:19:21 -0500

    footer beta

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import '../assets/styles/Footer.scss';
 import footerDecoration from '../assets/img/footerDecoration.png';
 import footerLogo from '../assets/img/logoFooter.png';
+import youTube from '../assets/img/youTube.png';
+import facebook from '../assets/img/facebook.png';
+import twitter from '../assets/img/twitter.png';
+import linkedln from '../assets/img/LinkedIn.png';
 
 
 const Footer = () =>(
@@ -14,8 +18,50 @@ const Footer = () =>(
             <div className="footer-item">
                 <img src={footerLogo} alt="" className="footer-logo"/>
             </div>
-            <div className="footer-item"></div>
-            <div className="footer-item"></div>
+            <div className="footer-item">
+                <div className="footer-nav">
+                    <ul>
+                        <li>
+                            <a href="">
+                            Inicio
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                            AMIA
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                            Comunicacion
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                            Estadisticas
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                            Suscriptores
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="footer-suscribe-container">
+                    <input type="text" placeholder="tuNombre@ejemplo.com"/>
+                    <p>Suscribete</p>
+                </div>
+            </div>
+            <div className="footer-item">
+                <p className="unete">Unete a la conversación</p>
+                <div className="redes-container">
+                    <img src={youTube} alt="" className="redes"/>
+                    <img src={facebook} alt="" className="redes"/>
+                    <img src={twitter} alt="" className="redes"/>
+                    <img src={linkedln} alt="" className="redes"/>
+                </div>
+            </div>
         </div>
     </footer>
 
